ff2e58b2f876d2ed58a8e41ab4fad81be3322de1
theonlywisdom <mailbaah@gmail.com>
2021-09-21 04:54:53 +0100

    Create Relationship Between Item and ApplicationUser Models

--- WishList/Models/ApplicationUser.cs
+++ WishList/Models/ApplicationUser.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 using WishList.Models.AccountViewModels;
 
 namespace WishList.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        
+        public virtual ICollection<Item> Items { get; set; }
     }
 }
